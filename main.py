f8756e0ece585f100b21730dc3e5534cf21cd843
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-08 19:44:52 +0100

    Similarity between phrases with vectors

--- main.py
+++ main.py
@@ -16,9 +16,23 @@ with open("data/wiki_us.txt", "r") as f:
 
 setence = list(doc.sents)[0]
 
-my_word = "country"
+doc1 = nlp("I like salty fries and hamburgers.")
+doc2 = nlp("Fast Food taste very good.")
+doc3 = nlp("The Empire State Building is in New-York.")
 
-ms = nlp.vocab.vectors.most_similar(np.asarray([nlp.vocab.vectors[nlp.vocab.strings[my_word]]]), n = 10)
-words = [nlp.vocab.strings[w] for w in ms[0][0]]
+print(doc1, "<->", doc2, doc1.similarity(doc2))
+print(doc1, "<->", doc3, doc1.similarity(doc3))
 
-print(words)
+doc4 = nlp("I enjoy oranges.")
+doc5 = nlp("I enjoy apples.")
+doc6 = nlp("I enjoy burgers.")
+
+print(doc4, "<->", doc5, doc4.similarity(doc5))
+print(doc4, "<->", doc6, doc4.similarity(doc6))
+
+#my_word = "country"
+
+#ms = nlp.vocab.vectors.most_similar(np.asarray([nlp.vocab.vectors[nlp.vocab.strings[my_word]]]), n = 10)
+#words = [nlp.vocab.strings[w] for w in ms[0][0]]
+
+#print(words)
