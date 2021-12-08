6f67beab4a6122fe5ffeaa956e7bde434d56040b
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-08 17:53:19 +0100

    Messing around with sentences (doc.sents)

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ for token in text[0:10]:
 
 for token in doc[:10]:
     print(token)
+
+for sent in doc.sents:
+    print(sent)
+
+sentence1 = list(doc.sents)[0]
+print(sentence1)
+sentence5 = list(doc.sents)[4]
+print(sentence5)
+print(len(list(doc.sents)))
+for token in sentence5:
+    print(token)
