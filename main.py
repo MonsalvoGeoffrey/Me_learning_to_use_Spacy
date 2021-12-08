671533f65d6161dd42e52cd78fdd8b2df7cb5235
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-08 17:06:33 +0100

    Checking how the doc object works

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
+doc = None
+
 with open("data/wiki_us.txt", "r") as f:
     text = f.read()
-    print(text)
+
+    doc = nlp(text)
+
+for token in text[0:10]:
+    print(token)
+
+for token in doc[:10]:
+    print(token)
