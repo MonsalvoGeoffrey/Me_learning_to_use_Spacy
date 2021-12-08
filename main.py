2b86a0f38465e0af8a3e94433e6f2492d61cbc0d
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-09 00:45:05 +0100

    Custom Pipes (remove_gpe)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy.language import Language
 from spacy.matcher import Matcher
 import json
 import numpy as np
@@ -8,7 +9,16 @@ nlp = spacy.load("en_core_web_sm")
 #nlp = spacy.load("en_core_web_md")
 #nlp = spacy.load("en_core_web_trf")
 
+@Language.component("remove_gpe")
+def remove_gpe(doc):
+    original_ents = list(doc.ents)
+    for ent in doc.ents:
+        if ent.label_ == "GPE":
+            original_ents.remove(ent)
+    doc.ents = original_ents
+    return doc
 
+nlp.add_pipe("remove_gpe")
 
 doc = None
 
@@ -16,3 +26,7 @@ with open("data/wiki_us.txt", "r") as f:
     text = f.read()
     doc = nlp(text)
 
+doc1 = nlp("Britain is a place. January is a doctor.")
+
+for ent in doc1.ents:
+    print(ent.text, ent.label_)
