4cd7f28f7ad6fe697f29ee75adbd2a2b7b88d878
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-08 18:54:19 +0100

    Cleaned up previous tests

--- main.py
+++ main.py
@@ -11,32 +11,3 @@ with open("data/wiki_us.txt", "r") as f:
     doc = nlp(text)
 
 
-sentence = list(doc.sents)[0]
-token3 = sentence[2]
-print(sentence)
-print(token3)
-print(token3.text)
-print(token3.left_edge)
-print(token3.right_edge)
-print(token3.ent_type)
-print(token3.ent_type_)
-print(token3.ent_iob)
-print(token3.ent_iob_)
-print(token3.lemma)
-print(token3.lemma_)
-print(sentence[12].lemma)
-print(sentence[12].lemma_)
-print(token3.morph)
-print(token3.pos_)
-print(sentence[12].pos_)
-print(token3.dep_)
-print(sentence[12].dep_)
-print(token3.lang_)
-
-text = "Mike enjoys playing football."
-doc2 = nlp(text)
-
-for token in doc2:
-    print(token.text, token.pos_, token.dep_)
-
-displacy.serve(sentence, style="dep")
