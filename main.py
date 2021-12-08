d72026bd8baf5d1f20e62a7779ef518f8ac6819d
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-08 23:13:55 +0100

    Entity Ruler Test

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import spacy
 import numpy as np
 from spacy import displacy
 
-#nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 #nlp = spacy.load("en_core_web_md")
 #nlp = spacy.load("en_core_web_trf")
 
-nlp = spacy.blank("en")
-nlp.add_pipe("sentencizer")
+
+ruler: spacy.pipeline.entityruler.EntityRuler = nlp.add_pipe("entity_ruler", before="ner")
+
+patterns = [
+    {"label": "GPE", "pattern": "West Chestertenfieldville"},
+    {"label": "FILM", "pattern": "Mr. Deeds"},
+]
+
+ruler.add_patterns(patterns)
 
 doc = None
 
@@ -17,5 +24,8 @@ with open("data/wiki_us.txt", "r") as f:
     doc = nlp(text)
 
 
-#displacy.serve(doc, style="ent")
-print(nlp.analyze_pipes())
+text = "West Chestertenfieldville was referenced in Mr. Deeds."
+doc1 = nlp(text)
+
+for ent in doc1.ents:
+    print(ent.text, ent.label_)
