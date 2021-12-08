bf8095404b66edc989a8fe16533cc102215ae9f4
Monsalvo Geoffrey <monsalvo.geoffrey+pro@gmail.com>
2021-12-08 17:55:24 +0100

    Cleanup previous tests

--- main.py
+++ main.py
@@ -9,19 +9,3 @@ with open("data/wiki_us.txt", "r") as f:
 
     doc = nlp(text)
 
-for token in text[0:10]:
-    print(token)
-
-for token in doc[:10]:
-    print(token)
-
-for sent in doc.sents:
-    print(sent)
-
-sentence1 = list(doc.sents)[0]
-print(sentence1)
-sentence5 = list(doc.sents)[4]
-print(sentence5)
-print(len(list(doc.sents)))
-for token in sentence5:
-    print(token)
